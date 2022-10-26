84ea3b1f6429d7a280684db6afab93c874d7e442
Pierre Marais <pierrem@live.co.za>
2022-10-26 19:15:59 +0200

    Implement finding negative weight cycles

--- src/main/scala/example/ArbitrageDetector.scala
+++ src/main/scala/example/ArbitrageDetector.scala
@@ -6,7 +6,7 @@ import scala.annotation.tailrec
 import scala.collection.mutable.ArrayBuffer
 
 trait ArbitrageDetector {
-  def detectArbitrage(graph: Graph): List[List[Currency]]
+  def detectArbitrage(graph: Graph): Set[List[Currency]]
 }
 
 object ArbitrageDetector {
@@ -14,7 +14,9 @@ object ArbitrageDetector {
 
   final case class CurrencyPricePair(from: Currency, to: Currency, rate: Double)
 
-  final case class Edge(from: Currency, to: Currency, weight: Double)
+  final case class Edge(from: Currency, to: Currency, weight: Double) {
+    def canRelax(distance: Map[Currency, Double]): Boolean = (distance(from) + weight) < distance(to)
+  }
 
   final case class Graph(graph: Map[Currency, List[Edge]]) {
     def addEdge(edge: Edge): Graph = {
@@ -49,22 +51,26 @@ object ArbitrageDetector {
 }
 
 class ArbitrageDetectorImpl() extends ArbitrageDetector {
-  override def detectArbitrage(g: Graph): List[List[Currency]] = {
+  override def detectArbitrage(g: Graph): Set[List[Currency]] = {
     val graph = g.graph
     val allEdges = graph.values.flatten
 
     println(s"graph $graph")
     println(s"edges $allEdges")
 
-    graph.keys.foldLeft[List[List[Currency]]](List()) { (acc, currency) =>
-      println(currency)
-      val bellmanFordOutput = bellmanFordAlgorithm(currency, g)
-      println(bellmanFordOutput)
-      acc
-    }
+//    graph.keys.foldLeft[Set[List[Currency]]](Set()) { (acc, currency) =>
+//      println(currency)
+//      val negativeWeightCycles = bellmanFordAlgorithm(currency, g)
+//      println(s"negativeWeightCycles: $negativeWeightCycles")
+//      negativeWeightCycles
+//    }
+    println("BTC")
+    val negativeWeightCycles = bellmanFordAlgorithm("BTC", g)
+    println(s"negativeWeightCycles: $negativeWeightCycles")
+    negativeWeightCycles
   }
 
-  def bellmanFordAlgorithm(sourceCurrency: Currency, g: Graph): BellmanFordOutput = {
+  def bellmanFordAlgorithm(sourceCurrency: Currency, g: Graph): Set[List[Currency]] = {
     val graph = g.graph
     val edges = graph.values.flatten
     val initialOutput = BellmanFordOutput(
@@ -72,16 +78,53 @@ class ArbitrageDetectorImpl() extends ArbitrageDetector {
       predecessor = graph.keys.map { k => (k, None) }.toMap
     )
 
-    (1 until graph.size).foldLeft(initialOutput) { (acc, _) =>
+    val outputAfterRelaxing = (1 until graph.size).foldLeft(initialOutput) { (acc, _) =>
       edges.foldLeft(acc) { (edgeAcc, edge) =>
-        val distance = edgeAcc.distance
-        val shouldRelax = (distance(edge.from) + edge.weight) < distance(edge.to)
-        if (shouldRelax) {
+        val canRelax = edge.canRelax(edgeAcc.distance)
+        if (canRelax) {
           edgeAcc.relaxEdge(edge)
         } else {
           edgeAcc
         }
       }
     }
+
+    println(s"Relaxing output: $outputAfterRelaxing")
+    findNegativeWeightCycles(g, outputAfterRelaxing)
+  }
+
+  def findNegativeWeightCycles(g: Graph, output: BellmanFordOutput): Set[List[Currency]] = {
+    val edges = g.graph.values.flatten
+    val seen = scala.collection.mutable.Set[Currency]()
+    val allCycles = scala.collection.mutable.Set[List[Currency]]()
+
+    edges.foreach{ e =>
+      var current = e.to
+      // If it's possible to relax further, it means that there must be a negative weight cycle,
+      // because it takes at maximum |V| - 1 relaxations to find the optimal value.
+      if (!seen.contains(current) && e.canRelax(output.distance)) {
+        val cycles = scala.collection.mutable.ArrayBuffer[Currency]()
+        var found = false
+        while(!found) {
+          seen.add(current)
+          cycles.append(current)
+          current = output.predecessor(e.to).getOrElse(throw new Exception("Expected to have a predecessor"))
+          if (current == e.to || cycles.contains(current)) {
+            println(s"current == e.to: ${current == e.to} || cycles.contains(current) ${cycles.contains(current)}")
+            found = true
+          }
+        }
+
+        val cycleStart = cycles.indexOf(current)
+        println(s"cycles: $cycles")
+
+        cycles.append(current)
+        println(s"cycles after append: $cycles")
+
+        allCycles.add(cycles.slice(cycleStart, cycles.size).reverse.toList)
+      }
+    }
+
+    allCycles.toSet
   }
 }
